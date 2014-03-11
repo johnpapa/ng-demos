de5ce6a8c6c5ecb84d8b3675ecf9f309f899ed68
John Papa <john@johnpapa.net>
2014-03-11 12:17:06 -0400

    added empty gulp file for starting a demo

---
+++ grunt-gulp/build-gulp/gulpfile_empty.js
@@ -0,0 +1,85 @@
+/*
+ * Create references
+ */
+
+
+
+
+/*
+ * Auto load all gulp plugins
+ */
+
+
+
+/*
+ * Load common utilities for gulp
+ */
+
+
+
+/*
+ * Create comments for minified files
+ */
+
+
+
+/*
+ * Could use a product/development switch.
+ * Run `gulp --production`
+ */
+
+
+
+
+/*
+ * Lint the code
+ */
+
+
+
+
+/*
+ * Minify and bundle the JavaScript
+ */
+
+
+
+
+
+/*
+ * Minify and bundle the CSS
+ */
+
+
+
+
+/*
+ * Compress images
+ */
+
+
+
+
+
+/*
+ * Bundle the JS, CSS, and compress images.
+ * Then copy files to production and show a toast.
+ */
+
+
+
+
+/*
+ * Remove all files from the output folder
+ */
+
+
+
+/*
+ * Watch file and re-run the linter
+ */
+
+
+
+
+
