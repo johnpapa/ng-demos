21360c3933faa4558593b6092b88e8d665afa528
John Papa <john@johnpapa.net>
2014-03-11 12:13:49 -0400

    added comments to gulp file and re-ordered them

--- grunt-gulp/build-gulp/gulpfile.js
+++ grunt-gulp/build-gulp/gulpfile.js
@@ -21,26 +21,67 @@ var gutil = plug.loadUtils(['colors', 'env', 'log', 'date']);
  */
 var commentHeader = common.createComments(gutil);
 
-// Run `gulp --production`
+/*
+ * Could use a product/development switch.
+ * Run `gulp --production`
+ */
 var type = gutil.env.production ? 'production' : 'development';
 gutil.log( 'Building for', gutil.colors.magenta(type) );
-//gutil.beep();
+gutil.beep();
 
-gulp.task('build-watcher', function() {
-    var jsWatcher = gulp.watch(pkg.paths.source.js, ['jshint']);
-
-    /*
-     * Rebuild when any files changes
-     */
-//    gulp.watch([pkg.paths.source.css,
-//        pkg.paths.source.js,
-//        pkg.paths.source.images], ['default']);
-
-    jsWatcher.on('change', function(event) {
-        console.log('*** File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
+/*
+ * Lint the code
+ */
+gulp.task('jshint', function () {
+    return gulp.src(pkg.paths.source.js)
+        .pipe(plug.jshint('jshintrc.json'))
+//        .pipe(plug.jshint.reporter('default'));
+        .pipe(plug.jshint.reporter('jshint-stylish'));
 });
 
+/*
+ * Minify and bundle the JavaScript
+ */
+gulp.task('bundlejs', ['jshint'], function () {
+    var bundlefile = pkg.name + ".min.js";
+    var opt = {newLine: ';'};
+
+    return gulp.src(pkg.paths.source.js)
+        .pipe(plug.size({showFiles: true}))
+        .pipe(plug.uglify())
+        .pipe(plug.concat(bundlefile, opt))
+        .pipe(plug.header(commentHeader))
+        .pipe(gulp.dest(pkg.paths.dest.js))
+        .pipe(plug.size({showFiles: true}));
+});
+
+
+/*
+ * Minify and bundle the CSS
+ */
+gulp.task('bundlecss', function () {
+    return gulp.src(pkg.paths.source.css)
+        .pipe(plug.size({showFiles: true}))
+        .pipe(plug.minifyCss({}))
+        .pipe(plug.concat(pkg.name + ".min.css"))
+        .pipe(plug.header(commentHeader))
+        .pipe(gulp.dest(pkg.paths.dest.css))
+        .pipe(plug.size({showFiles: true}));
+});
+
+/*
+ * Compress images
+ */
+gulp.task('images', function () {
+    return gulp.src(pkg.paths.source.images)
+        .pipe(plug.cache(plug.imagemin({optimizationLevel: 3})))
+        .pipe(gulp.dest(pkg.paths.dest.images));
+});
+
+/*
+ * Bundle the JS, CSS, and compress images.
+ * Then copy files to production and show a toast.
+ */
 gulp.task('default', ['bundlejs', 'bundlecss', 'images'], function () {
     // Copy the CSS to prod
     return gulp.src(pkg.paths.dest.css + '/**/*')
@@ -57,46 +98,30 @@ gulp.task('default', ['bundlejs', 'bundlecss', 'images'], function () {
         }));
 });
 
-gulp.task('bundlecss', function () {
-    return gulp.src(pkg.paths.source.css)
-        .pipe(plug.size({showFiles: true}))
-        .pipe(plug.minifyCss({}))
-        .pipe(plug.concat(pkg.name + ".min.css"))
-        .pipe(plug.header(commentHeader))
-        .pipe(gulp.dest(pkg.paths.dest.css))
-        .pipe(plug.size({showFiles: true}));
-});
-
-gulp.task('bundlejs', ['jshint'], function () {
-    var bundlefile = pkg.name + ".min.js";
-    var opt = {newLine: ';'};
-
-    return gulp.src(pkg.paths.source.js)
-        .pipe(plug.size({showFiles: true}))
-        .pipe(plug.uglify())
-        .pipe(plug.concat(bundlefile, opt))
-        .pipe(plug.header(commentHeader))
-        .pipe(gulp.dest(pkg.paths.dest.js))
-        .pipe(plug.size({showFiles: true}));
-});
-
-// format c: /y
+/*
+ * Remove all files from the output folder
+ */
 gulp.task('cleanOutput', function(){
     return gulp.src([
             pkg.paths.dest.base,
             pkg.paths.production])
         .pipe(plug.clean({force: true}))
 });
+/*
+ * Watch file and re-run the linter
+ */
+gulp.task('build-watcher', function() {
+    var jsWatcher = gulp.watch(pkg.paths.source.js, ['jshint']);
 
-gulp.task('images', function () {
-    return gulp.src(pkg.paths.source.images)
-        .pipe(plug.cache(plug.imagemin({optimizationLevel: 3})))
-        .pipe(gulp.dest(pkg.paths.dest.images));
+    /*
+     * Rebuild when any files changes
+     */
+//    gulp.watch([pkg.paths.source.css,
+//        pkg.paths.source.js,
+//        pkg.paths.source.images], ['default']);
+
+    jsWatcher.on('change', function(event) {
+        console.log('*** File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
 });
 
-gulp.task('jshint', function () {
-    return gulp.src(pkg.paths.source.js)
-        .pipe(plug.jshint('jshintrc.json'))
-//        .pipe(plug.jshint.reporter('default'));
-        .pipe(plug.jshint.reporter('jshint-stylish'));
-});
