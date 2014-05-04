a16f6977598a09da178e6ff134881570e4322158
John Papa <john@johnpapa.net>
2014-05-04 12:15:30 -0400

    dashboard does depend on avengers module, because it needs its data services. if we abstract those, then we can adjust this.

--- modular/app/dashboard/app.js
+++ modular/app/dashboard/app.js
@@ -1,7 +1,10 @@
 (function () {
     'use strict';
 
-    var app = angular.module('modularApp.dashboard', ['modularApp.core']);
+    var app = angular.module('modularApp.dashboard',
+        ['modularApp.core', 'modularApp.avengers']
+    );
+
     app.run(['$route', 'dashboard.routes', function($route, routes) {
         $route.routeStore = $route.routeStore.concat(routes);
     }]);
