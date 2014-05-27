761654706d4b0af4d921c2f20ba500477d9aa2f8
John Papa <john@johnpapa.net>
2014-05-26 21:52:08 -0400

    cc-bmean now works!

--- cc-bmean/app/app.js
+++ cc-bmean/app/app.js
@@ -14,8 +14,8 @@
 
         // 3rd Party Modules
         'ui.bootstrap',     // ui-bootstrap (ex: carousel, pagination, dialog)
-        'breeze.angular', // tells breeze to use $q instead of Q.js
-        'breeze.directives', // breeze validation directive (zValidate)
+        'breeze.angular',   // tells breeze to use $q instead of Q.js
+        'breeze.directives',// breeze validation directive (zValidate)
         'ngplus',           // ngplus utilities
         'ngzWip'            // zStorage and zStorageWip
     ]);
