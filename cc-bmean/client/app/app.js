7efd715651824983c5f664685a6436a8374b3a91
John Papa <john@johnpapa.net>
2014-07-04 15:17:11 -0400

    modular: comments explaining the module dependency chain

--- cc-bmean/client/app/app.js
+++ cc-bmean/client/app/app.js
@@ -10,7 +10,9 @@
          */
 
         /*
-         * Everybody has access to these
+         * Everybody has access to these.
+         * We could place these under every feature area,
+         * but this is easier to maintain.
          */
         'app.core',
         'app.data', // needs core
