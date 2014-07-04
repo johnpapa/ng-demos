b82ecef7ea489e8eb2406e5772df20759d1f610c
John Papa <john@johnpapa.net>
2014-07-04 15:17:00 -0400

    cc-bmean: comments explaining the module dependency chain

--- modular/client/app/app.module.js
+++ modular/client/app/app.module.js
@@ -2,10 +2,27 @@
     'use strict';
 
     angular.module('app', [
+        /*
+         * Order is not important. Angular makes a
+         * pass to register all of the modules listed
+         * and then when app.dashboard tries to use app.data,
+         * it's components are available.
+         */
+
+        /*
+         * Everybody has access to these.
+         * We could place these under every feature area,
+         * but this is easier to maintain.
+         */
+        'app.core',
+        'app.widgets',
+
+        /*
+         * Feature areas
+         */
         'app.avengers',
         'app.dashboard',
-        'app.layout',
-        'app.widgets'
+        'app.layout'
     ]);
 
 })();
