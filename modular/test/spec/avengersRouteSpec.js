34b77e4ba227bc370cc0ae8631e0b3ac38c337a3
John Papa <john@johnpapa.net>
2014-07-14 23:57:49 -0400

    modular: added basic tests for routes and sidebar

---
+++ modular/test/spec/avengersRouteSpec.js
@@ -0,0 +1,42 @@
+describe('avengers', function () {
+    describe('route', function () {
+        var $controller,
+            dataservice,
+            $httpBackend,
+            $location,
+            $q,
+            $rootScope,
+            $route,
+            scope,
+            toastr;
+
+        beforeEach(function () {
+            module('app');
+            inject(function (_$controller_, _$httpBackend_, _$location_, _$q_, _$rootScope_, _$route_, _dataservice_, _toastr_) {
+                $controller = _$controller_;
+                $httpBackend = _$httpBackend_;
+                $location = _$location_;
+                $q = _$q_;
+                $rootScope = _$rootScope_;
+                $route = _$route_;
+                dataservice = _dataservice_;
+                toastr = _toastr_;
+            });
+        });
+
+        beforeEach(function () {
+            $httpBackend.expectGET('/app/avengers/avengers.html').respond(200);
+        });
+
+        it('should map /avengers route to avengers View template', function () {
+            expect($route.routes['/avengers'].templateUrl).
+                toEqual('/app/avengers/avengers.html');
+        });
+
+        it('should route / to the avengers View', function () {
+            $location.path('/avengers');
+            $rootScope.$apply();
+            expect($route.current.templateUrl).toEqual('/app/avengers/avengers.html');
+        });
+    });
+});
--- modular/test/spec/dashboardSpec.js
+++ modular/test/spec/dashboardSpec.js
@@ -1,63 +1,69 @@
-describe('Dashboard', function () {
-    var $controller,
-        dataservice,
-        $httpBackend,
-        $q,
-        $rootScope,
-        scope,
-        toastr;
+describe('dashboard', function () {
+    describe('dashboard', function () {
+        var $controller,
+            dataservice,
+            $httpBackend,
+            $location,
+            $q,
+            $rootScope,
+            $route,
+            scope,
+            toastr;
 
-    beforeEach(function () {
-        module('app');
-        inject(function (_$controller_, _$httpBackend_, _$q_, _$rootScope_, _dataservice_, _toastr_) {
-            $controller = _$controller_;
-            $httpBackend = _$httpBackend_;
-            $q = _$q_;
-            $rootScope = _$rootScope_;
-            dataservice = _dataservice_;
-            toastr = _toastr_;
+        beforeEach(function () {
+            module('app');
+            inject(function (_$controller_, _$httpBackend_, _$location_, _$q_, _$rootScope_, _$route_, _dataservice_, _toastr_) {
+                $controller = _$controller_;
+                $httpBackend = _$httpBackend_;
+                $location = _$location_;
+                $q = _$q_;
+                $rootScope = _$rootScope_;
+                $route = _$route_;
+                dataservice = _dataservice_;
+                toastr = _toastr_;
+            });
         });
-    });
 
-    beforeEach(function () {
-        $httpBackend.when('GET', '/app/dashboard/dashboard.html').respond(200);
+        beforeEach(function () {
+            $httpBackend.when('GET', '/app/dashboard/dashboard.html').respond(200);
 //        $httpBackend.expectGET(/\w+.html/).respond(200, '');
-        $httpBackend.flush();
+            $httpBackend.flush();
 
-        spyOn(dataservice, 'getAvengerCount').and.callFake(function () {
-            var deferred = $q.defer();
-            deferred.resolve(testctx.getMockAvengers().length);
-            return deferred.promise;
+            spyOn(dataservice, 'getAvengerCount').and.callFake(function () {
+                var deferred = $q.defer();
+                deferred.resolve(testctx.getMockAvengers().length);
+                return deferred.promise;
+            });
+
+            spyOn(dataservice, 'ready').and.callFake(function () {
+                var deferred = $q.defer();
+                deferred.resolve({test: 123});
+                return deferred.promise;
+            });
         });
 
-        spyOn(dataservice, 'ready').and.callFake(function () {
-            var deferred = $q.defer();
-            deferred.resolve({test: 123});
-            return deferred.promise;
+
+        beforeEach(inject(function ($rootScope, $controller) {
+            scope = $rootScope.$new();
+            controller = $controller('Dashboard as vm', {
+                '$scope': scope,
+                'dataservice': dataservice
+            });
+        }));
+
+        it('should have title of Dashboard', function () {
+            $rootScope.$apply();
+            expect(scope.vm.title).toEqual('Dashboard');
         });
-    });
 
-
-    beforeEach(inject(function ($rootScope, $controller) {
-        scope = $rootScope.$new();
-        controller = $controller('Dashboard as vm', {
-            '$scope': scope,
-            'dataservice': dataservice
+        it('should have Avenger Count of 5', function () {
+            $rootScope.$apply();
+            expect(scope.vm.avengerCount).toEqual(5);
         });
-    }));
 
-    it('should have title of Dashboard', function () {
-        $rootScope.$apply();
-        expect(scope.vm.title).toEqual('Dashboard');
-    });
-
-    it('should have Avenger Count of 5', function () {
-        $rootScope.$apply();
-        expect(scope.vm.avengerCount).toEqual(5);
-    });
-
-    afterEach (function () {
-        $httpBackend.verifyNoOutstandingExpectation();
-        $httpBackend.verifyNoOutstandingRequest();
+        afterEach(function () {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
     });
 });
