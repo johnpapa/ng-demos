2261b815680657be5f9b9b3d851754a9f191c8ec
John Papa <john@johnpapa.net>
2014-07-29 09:01:48 -0400

    cc-bmean: added route resolver example that gets injected into controller. in comments

--- cc-bmean/client/app/dashboard/config.route.js
+++ cc-bmean/client/app/dashboard/config.route.js
@@ -14,15 +14,19 @@
                 config: {
                     templateUrl: 'app/dashboard/dashboard.html',
                     title: 'dashboard',
+
                     // TODO: This helps us test adding a resolver to a route
                     // and it is added to the queue of resolvers
+
+                    /*
+                     * If we specify the controller and
+                     * controllerAs in the route we can inject
+                     * this resolver into the controller, too.
+                     */
+                    /*
+                    controller: 'Dashboard',
+                    controllerAs: 'vm',
                     resolve: {
-                        /*
-                         * If we specify the controller and
-                         * controllerAs in the route we can inject
-                         * this resolver into the controller, too.
-                         */
-                        /*
                         toasty: function($q) {
                             var deferred = $q.defer();
                             var people =[
@@ -34,8 +38,8 @@
                             toastr.warning('yes, yes i do');
                             return deferred.promise;
                         }
-                        */
                     },
+                    */
                     settings: {
                         nav: 1,
                         content: '<i class="fa fa-dashboard"></i> Dashboard'
