dae3b4b309bf71a05086bcfc9aaeecb8a7998f72
John Papa <john@johnpapa.net>
2014-09-11 20:48:24 -0400

    modular: added injection helper to spechelper

--- modular/src/client/test/spec.mocha/avengers.spec.js
+++ modular/src/client/test/spec.mocha/avengers.spec.js
@@ -1,26 +1,16 @@
 /*global sinon, describe, it, afterEach, beforeEach, expect, inject, testctx */
-describe('avengers', function () {
-    var $controller,
-        dataservice,
-        $httpBackend,
-        $location,
-        $q,
-        $rootScope,
-        $route,
-        scope,
-		controller,
-        toastr;
+describe.only('avengers', function() {
+    var deps;
+    var dataservice;
+    var scope;
+    var controller;
+    var toastr;
 
-    beforeEach(function () {
+    beforeEach(function() {
         module('app', testctx.fakeLogger);
-        inject(function (_$controller_, _$httpBackend_, _$location_, 
-						  _$q_, _$rootScope_, _$route_, _dataservice_, _toastr_) {
-            $controller = _$controller_;
-            $httpBackend = _$httpBackend_;
-            $location = _$location_;
-            $q = _$q_;
-            $rootScope = _$rootScope_;
-            $route = _$route_;
+        deps = testctx.injectDependencies(true);
+
+        inject(function(_dataservice_, _toastr_) {
             dataservice = _dataservice_;
             toastr = _toastr_;
         });
@@ -31,38 +21,35 @@ describe('avengers', function () {
 //        $httpBackend.expectGET(/\w+.html/).respond(200, '');
         $httpBackend.flush();
 
-        sinon.stub(dataservice, 'getAvengers', function () {
+        sinon.stub(dataservice, 'getAvengers', function() {
             var deferred = $q.defer();
             deferred.resolve(testctx.getMockAvengers());
             return deferred.promise;
         });
 
-        sinon.stub(dataservice, 'ready', function () {
+        sinon.stub(dataservice, 'ready', function() {
             var deferred = $q.defer();
             deferred.resolve({test: 123});
             return deferred.promise;
         });
 
-        scope = $rootScope.$new();
-        controller = $controller('Avengers as vm', {
-            '$scope': scope
-        });
+        controller = $controller('Avengers');
         $rootScope.$apply();
     });
 
-    it('should have Avengers controller', function () {
+    it('should have Avengers controller', function() {
         expect(controller).not.to.equal(null);
     });
 
-    it('should have title of Avengers', function () {
-        expect(scope.vm.title).to.equal('Avengers');
+    it('should have title of Avengers', function() {
+        expect(controller.title).to.equal('Avengers');
     });
 
-    it('should have 5 Avengers', function () {
-        expect(scope.vm.avengers.length).to.equal(5);
+    it('should have 5 Avengers', function() {
+        expect(controller.avengers.length).to.equal(5);
     });
 
-    afterEach(function () {
+    afterEach(function() {
         $httpBackend.verifyNoOutstandingExpectation();
         $httpBackend.verifyNoOutstandingRequest();
     });
