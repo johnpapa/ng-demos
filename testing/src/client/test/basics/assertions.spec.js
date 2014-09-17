a5514bca4562d6cfd42bfe85c6366ad6716a1545
Ward Bell <wardbell@hotmail.com>
2014-09-17 16:15:47 -0700

    modular: added some basic assertion tests for exceptions

--- testing/src/client/test/basics/assertions.spec.js
+++ testing/src/client/test/basics/assertions.spec.js
@@ -6,7 +6,38 @@ describe('Basics - assertion examples:', function () {
         it('should return -1 when the value is not present', function () {
             expect([1, 2, 3].indexOf(5)).to.be.equal(-1);
         });
-
     });
 
+
+
+
+    describe('when expecting exceptions', function () {
+        it('passes only if there is an exception', function () {
+            expect(function () {
+                throw new Error('my bad');
+            }).to.throw();
+        });
+
+        it('of a particular Error type', function () {
+            // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ReferenceError
+            var err = new ReferenceError('This is a bad reference.');
+            expect(function () {
+                throw err;
+            }).to.throw(ReferenceError);
+        });
+
+        it('with a particular message', function () {
+            expect(function () {
+                throw new Error('my bad');
+            }).to.throw(/bad/);
+        });
+
+
+        it('of a particular Error type with a specific message', function () {
+            var err = new ReferenceError('This is a bad reference.');
+            expect(function () {
+                throw err;
+            }).to.throw(ReferenceError, /bad reference/);
+        });
+    });
 });
