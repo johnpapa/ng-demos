2a2c164ee9f5ad9a7b2bf45ebbb48bac761e59da
johnpapa <john@johnpapa.net>
2014-11-09 10:38:38 -0800

    1.3: removed dead code

--- ng-1.3 playground/src/client/app/dashboard/dashboard.controller.js
+++ ng-1.3 playground/src/client/app/dashboard/dashboard.controller.js
@@ -6,39 +6,16 @@
         .controller('Dashboard', Dashboard);
 
     /* @ngInject */
-    function Dashboard($q, dataservice, logger) {
+    function Dashboard(logger) {
         /*jshint validthis: true */
         var vm = this;
 
-        vm.news = {
-            title: 'Marvel Avengers',
-            description: 'Marvel Avengers 2 is now in production!'
-        };
-        vm.avengerCount = 0;
-        vm.avengers = [];
         vm.title = 'Dashboard';
 
         activate();
 
         function activate() {
-            var promises = [getAvengerCount(), getAvengersCast()];
-            return $q.all(promises).then(function() {
-                logger.info('Activated Dashboard View');
-            });
-        }
-
-        function getAvengerCount() {
-            return dataservice.getAvengerCount().then(function(data) {
-                vm.avengerCount = data;
-                return vm.avengerCount;
-            });
-        }
-
-        function getAvengersCast() {
-            return dataservice.getAvengersCast().then(function(data) {
-                vm.avengers = data;
-                return vm.avengers;
-            });
+            logger.info('Activated Dashboard View');
         }
     }
 })();
