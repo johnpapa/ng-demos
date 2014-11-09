7ae1011c6dd8b70ad468ee60bd8fb6eaf64e2cbd
John Papa <john@johnpapa.net>
2014-11-08 23:43:42 -0800

    added customers data

--- ng-1.3 playground/src/server/routes/index.js
+++ ng-1.3 playground/src/server/routes/index.js
@@ -3,6 +3,12 @@ module.exports = function(app) {
     var jsonfileservice = require('./utils/jsonfileservice')();
 
     app.get('/api/maa', getMaa);
+    app.get('/api/customers', getCustomers);
+
+    function getCustomers(req, res, next) {
+        var json = jsonfileservice.getJsonFromFile(data + 'customers.json');
+        res.send(json);
+    }
 
     function getMaa(req, res, next) {
         var json = jsonfileservice.getJsonFromFile(data + 'maa.json');
